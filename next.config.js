e23fe0a538c17139b33550ce037fc89a3e94a9c0
Ghub2709 <info@premium-energiepass.online>
2024-11-18 03:52:57 +0100

    Fix deployment configuration and add debug step

--- next.config.js
+++ next.config.js
@@ -1,28 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/epass' : '',
   images: {
     unoptimized: true
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/epass' : '',
-  webpack: (config) => {
-    config.cache = false
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/videos/',
-          outputPath: 'static/videos/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    })
-    return config
-  }
 }
 
 module.exports = nextConfig
