5b316409671c10b84eae6c42c4d82992ee0f34e8
Ghub2709 <info@premium-energiepass.online>
2024-11-19 05:25:07 +0100

    Changed next.config.js to serve from the base path and not from epass anymore

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/epass' : '',
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/epass' : '',
 }
 
 module.exports = nextConfig
