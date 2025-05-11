d2b8ea25254f49786fd14e2caacc636afc0396ea
Ghub2709 <info@premium-energiepass.online>
2025-05-11 09:56:35 +0200

    2.311 GTM Verbesserungen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// GTM ID
+const GTM_ID = 'GTM-MH42RVCS'
+
 export default function RootLayout({
   children,
 }: {
@@ -37,27 +40,29 @@ export default function RootLayout({
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MH42RVCS');`
-          }}
-        />
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MH42RVCS"
-            height="0" 
-            width="0" 
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <ScrollToTop />
       </body>
