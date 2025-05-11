b7e3342e830a7eaa4e281cc41acbd9d6c5edc8aa
Ghub2709 <info@premium-energiepass.online>
2025-05-11 09:20:34 +0200

    Version 2.31 Google Tag Manager verbessert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ScrollToTop from '@/components/ScrollToTop'
 import { useEffect } from 'react'
 import { metadata } from './metadata'
 import emailjs from '@emailjs/browser';
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,25 +35,29 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MH42RVCS');`
-        }} />
-        {/* End Google Tag Manager */}
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MH42RVCS');`
+          }}
+        />
       </head>
       <body className={`${inter.className} antialiased`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH42RVCS"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-        }} />
-        {/* End Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MH42RVCS"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <ScrollToTop />
       </body>
