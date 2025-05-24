7d07469626946e93ac43cc745e16e67b88152668
Ghub2709 <info@premium-energiepass.online>
2025-05-24 05:38:54 +0200

    Version 3.011 Favicon funktioniert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon-v2.png" type="image/png" />
       </head>
       <body className={`${inter.className} antialiased`}>
         <GTM />
